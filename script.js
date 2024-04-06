49bd6597b90425a51fec08f4e978772f93acab4c
Ganeshdarshan Venkatraman Bhat <88202395+bhatganeshdarshan@users.noreply.github.com>
2024-04-06 18:32:25 +0530

    ui updated

--- script.js
+++ script.js
@@ -145,7 +145,7 @@
             ctx.beginPath();
             ctx.moveTo(p.x, p.y);
             ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.strokeStyle = 'rgba(139,147,255,'+ p.active+')';
             ctx.stroke();
         }
     }
@@ -163,7 +163,7 @@
             if(!_this.active) return;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fillStyle = 'rgba(139,147,255,'+ _this.active+')';
             ctx.fill();
         };
     }
